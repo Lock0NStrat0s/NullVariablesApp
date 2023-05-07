718de35348dbe6c56c40f35829bc8e24e0520a7a
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-07 17:15:32 -0600

    review complete.

--- NullVariables/Program.cs
+++ NullVariables/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// null - lack of value
+
+// haven't asked for the age yet
+int? age = null;
+bool? birthday = null;
+double? battingAverage = null;
+string firstName = null;
+
+// we have asked for the age now
+age = 0;
+Console.WriteLine(age);
